c65723e17b9a0c3be8b949fd694feaafba52c6da
sankardharani <sankarlaldream@gmail.com>
2026-09-17 21:11:15 +0530

    remove files for branch

--- server/src/employee.ts
+++
@@ -1,9 +0,0 @@
-import * as mongodb from "mongodb";
-
-export interface Employee {
-    name: string;
-    position: string;
-    unique_id: number;
-    level: "junior" | "mid" | "senior";
-    _id?: mongodb.ObjectId;
-}
